53445202efcac51f397c09e74e8697e05e44354a
Matia-k <matia40k@gmail.com>
2022-09-20 13:23:40 -0700

    corrected handling of negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 string_number = str(number)
+if string_number[0] == '-':
+    sign = -1
+else:
+    sign == 1
+
 len_number = len(string_number)
-digit = int(string_number[len_number - 1])
+digit = int(string_number[len_number - 1]) * sign
 
 if digit > 5:
     print(f"Last digit of {number} is {digit} and is greater than 5")
@@ -11,4 +16,3 @@ elif digit == 0:
     print(f"Last digit of {number} is {digit} and is 0")
 else:
     print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
-
