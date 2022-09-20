05857cdb53129d2d54bdf4093f14e72367e9974d
Matia-k <matia40k@gmail.com>
2022-09-20 13:30:10 -0700

    corrected handling of negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,8 +11,8 @@ len_number = len(string_number)
 digit = int(string_number[len_number - 1]) * sign
 
 if digit > 5:
-    print(f"Last digit of {number} is {digit} and is greater than 5")
+    print(f"Last digit of {number:d} is {digit:d} and is greater than 5")
 elif digit == 0:
-    print(f"Last digit of {number} is {digit} and is 0")
+    print(f"Last digit of {number:d} is {digit:d} and is 0")
 else:
-    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {digit:d} and is less than 6 and not 0")
