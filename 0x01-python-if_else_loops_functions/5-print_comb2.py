2829c921616b224c589e8e01c5719b9b9d4891ab
Matia-k <matia40k@gmail.com>
2022-09-20 15:09:33 -0700

    completed task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 99):
     if n < 10:
-        print("0{}".format(n), end=", ")
-    else:
-        print("{}".format(n), end=", ")
+        n = '0' + str(n)
+    print("{}".format(n), end=", ")
 print("99")
