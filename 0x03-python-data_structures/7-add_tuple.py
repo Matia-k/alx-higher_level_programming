1845a73f1478ffb18ea0216b69b63a029bd3e80a
Matia-k <matia40k@gmail.com>
2022-09-29 12:00:47 -0700

    completed task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_b) == 0:
+        added = (tuple_a[0] + 0, tuple_a[1] + 0)
+    elif len(tuple_a) == 1:
+        added = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+    elif len(tuple_b) == 1:
+        added = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+    else:
+        added = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return added
