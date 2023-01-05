a9d4db5dfcffa18acdba6025a0220c9c4ae907b6
Matia-k <matia40k@gmail.com>
2023-01-05 02:55:54 -0800

    completed task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dict = a_dictionary.copy()
-    if key in new_dict:
-        del new_dict[key]
-        return new_dict
+    if key in a_dictionary:
+        del a_dictionary[key]
+        return a_dictionary
     else:
         return a_dictionary
