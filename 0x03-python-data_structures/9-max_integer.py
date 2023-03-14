dc9f54ef4297e1e3a62abcb7f6f9c916345f29f4
Matia-k <matia40k@gmail.com>
2023-03-14 01:03:54 -0700

    handled list of negative values

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    bigest = 0
     list_len = len(my_list)
     i = 0
 
     if list_len == 0:
         return None
-
+    
+    bigest = my_list[0]
     while (i < list_len):
         if my_list[i] > bigest:
             bigest = my_list[i]
