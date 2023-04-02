2b39337ac2e137ba0f6fe5b9c72ee313619296aa
Matia-k <matia40k@gmail.com>
2023-04-02 08:02:23 -0700

    completed tasks 0,1,2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a class Square"""
+
+    def __init__(self, size):
+        """Initializes a new Squre.
+
+        Args:
+            size(int): the size of the new squre.
+        """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0):
+        """Initializes a new square.
+
+        Args:
+            size(int): size of the new square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
