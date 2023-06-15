1dbcab183dcd9501cf273f877e2172daa05f4945
Matia-k <matia40k@gmail.com>
2023-06-15 00:36:24 -0700

    corrected output of str function

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,3 +15,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return ("[" + str(self.__class__.__name__) + "] " + str(self.__width) +
+        "/" + str(self.__height))
