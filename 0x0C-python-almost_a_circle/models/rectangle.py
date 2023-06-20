ac4c6213007060c9f0c8e2b89873bfc153de0b05
Matia-k <matia40k@gmail.com>
2023-06-20 14:19:37 -0700

    task 7 completed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,11 @@ class Rectangle(Base):
 
     def display(self):
         """prints the rectangle instance with character #"""
+        for y in range(self.y):
+            print()
         for h in range(self.height):
+            for x in range(self.x):
+                print(" ", end="")
             for w in range(self.width):
                 print("#", end="")
             print()
