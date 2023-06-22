3f15027a2b9ae0b0709c336d12f749aa43267a1c
Matia-k <matia40k@gmail.com>
2023-06-22 12:28:27 -0700

    completed tasks 11 - 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,3 +30,42 @@ class Square(Rectangle):
         """Returns the str representation of a square"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
+
+    def update(self, *args, **kwargs):
+        """updates the attributes of the square.
+
+        Args:
+            *args(tuple): a tuple of arguments to assign as attributes.
+            **kwargs(dict): key-value pairs of the new attributes.
+        """
+        if len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+        elif (not args) or len(args) == 0 and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a square"""
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
