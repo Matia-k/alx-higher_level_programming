dd99ff9423e862a60cd7180562d511d29e2c5484
Matia-k <matia40k@gmail.com>
2023-06-23 02:06:46 -0700

    added mehtod that returns json representation of object

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a base class"""
-
+import json
+import csv
 
 class Base:
     """A class that will be the “base” of all other classes in this project.
@@ -20,3 +21,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries(list): a list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs(list): list of instances which inherits from base.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jfile:
+            if list_objs is None:
+                jfile.write("[]")
+            else:
+                list_of_dicts = [obj.to_dictionary() for obj in list_objs]
+                jfile.write(Base.to_json_string(list_of_dicts))
