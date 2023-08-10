f88731181c94ba2a4bd5a9d8ef774eaa557f2a0e
Matia-k <matia40k@gmail.com>
2023-08-10 01:38:12 -0700

    task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Creates a table in the current database in a MySQL server.
+-- Scripts should not fail if the table already exitst.
+CREATE IF NOT EXISTS first_table (
+id int,
+name VARCHAR(256));
