32caa4d998e649dadce8f30c71a445be50cfff7a
Matia-k <matia40k@gmail.com>
2023-09-13 06:08:40 -0700

    Corrected error in parsing argument to function

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,11 +4,11 @@ module.exports = class Square extends require('./5-square') {
     super(size, size);
   }
 
-  charPrint (c) {
-    if (c === undefined) this.print();
+  charPrint (C) {
+    if (C === undefined) this.print();
     else {
       for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) process.stdout.write('C');
+        for (let j = 0; j < this.width; j++) process.stdout.write(C);
         console.log();
       }
     }
